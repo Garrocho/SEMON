7a8d3943206a684a85a694546c04a8412c269208
Charles Garrocho <ctgarrocho@gmail.com>
2012-10-14 12:36:42 -0300

    Alterado: Para as normas da PEP8.

--- detector.py
+++ detector.py
@@ -6,6 +6,7 @@ import cv2.cv as cv
 from datetime import datetime
 from sys import exit
 
+
 class DetectorMovimentos():
     """
     Esta classe permite a deteccao de movimentos de uma determinada webcam.
@@ -15,12 +16,13 @@ class DetectorMovimentos():
         """
         O construtor obtem a referencia da webcam e cria uma janela para exibir as imagens.
         """
+
         # Criando uma janela com o nome de "webCam". Sera utilizada para exibir as imagens da webCam.
         cv.NamedWindow('webCam')
- 
+
         # Obtendo a referencia da captura da webCam.
         self.webCam = cv.CaptureFromCAM(0)
-        
+
         # Obtendo a imagem atual da webCam.
         self.imagem_atual = cv.QueryFrame(self.webCam)
 
@@ -28,31 +30,31 @@ class DetectorMovimentos():
             print 'A Web Cam esta desligada. Por favor ligue-a'
             exit()
         else:
-		    # Cria uma nova imagem que sera utilizada para descobrir os contornos na imagem_atual.
-		    self.imagem_cinza = cv.CreateImage(cv.GetSize(self.imagem_atual), cv.IPL_DEPTH_8U, 1)
+            # Cria uma nova imagem que sera utilizada para descobrir os contornos na imagem_atual.
+            self.imagem_cinza = cv.CreateImage(cv.GetSize(self.imagem_atual), cv.IPL_DEPTH_8U, 1)
 
-		    # Cria uma nova imagem que sera utilizada para converter a imagem atual em 32F.
-		    self.imagem_auxiliar = cv.CreateImage(cv.GetSize(self.imagem_atual), cv.IPL_DEPTH_32F, 3)
+            # Cria uma nova imagem que sera utilizada para converter a imagem atual em 32F.
+            self.imagem_auxiliar = cv.CreateImage(cv.GetSize(self.imagem_atual), cv.IPL_DEPTH_32F, 3)
 
-		    # Imagem sera utilizada para guardar a diferenca entre a imagem atual e anterior.
-		    self.imagem_diferenca = None
+            # Imagem sera utilizada para guardar a diferenca entre a imagem atual e anterior.
+            self.imagem_diferenca = None
 
-		    # Obtendo a area total da imagem da webCam.
-		    self.area = self.imagem_atual.width * self.imagem_atual.height
-		    self.area_corrente = 0
+            # Obtendo a area total da imagem da webCam.
+            self.area = self.imagem_atual.width * self.imagem_atual.height
+            self.area_corrente = 0
 
-		    self.imagem_diferenca = cv.CloneImage(self.imagem_atual)
-		    self.imagem_anterior = cv.CloneImage(self.imagem_atual)
+            self.imagem_diferenca = cv.CloneImage(self.imagem_atual)
+            self.imagem_anterior = cv.CloneImage(self.imagem_atual)
 
-		    # Tenho que converter a imagem_atual em 32F para poder calcular a media em "RuningAvg".
-		    cv.Convert(self.imagem_atual, self.imagem_auxiliar)
-
+            # Tenho que converter a imagem_atual em 32F para poder calcular a media em "RuningAvg".
+            cv.Convert(self.imagem_atual, self.imagem_auxiliar)
 
     def processaImagem(self):
         """
         Crio uma imagem cinza a partir da atual para o programa ficar mais rapido, crio uma imagem com a
         diferenca da imagem anterior e a imagem atual, e binarizo a imagem cinza para filtrar pixels pequenos.
         """
+
         # Obtendo a imagem atual da webCam.
         self.imagem_atual = cv.QueryFrame(self.webCam)
 
@@ -72,14 +74,14 @@ class DetectorMovimentos():
         cv.CvtColor(self.imagem_diferenca, self.imagem_cinza, cv.CV_RGB2GRAY)
 
         # Binariza a imagem. Para poder filtrar pixels pequenos.
-        cv.Threshold(self.imagem_cinza, self.imagem_cinza, 50, 255, cv.CV_THRESH_BINARY)  
+        cv.Threshold(self.imagem_cinza, self.imagem_cinza, 50, 255, cv.CV_THRESH_BINARY)
 
-   
     def verificaMovimento(self):
         """
         Obtem os contornos da imagem cinza e soma a area deles para verificar se ouve diferenca.
         Caso a soma da area dos contornos seja maior que o "0" retorna True, caso contrario False.
         """
+
         # Encontra os contornos dos objetos na imagem cinza.
         contornos = cv.FindContours(self.imagem_cinza, cv.CreateMemStorage(0))
 
